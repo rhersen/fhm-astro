a50ac57360b5d6ed03e931f0470a48a2ba9f0297
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-08-27 12:10:17 +0200

    fetch fhm data

---
+++ src/components/Latest.jsx
@@ -0,0 +1,9 @@
+export default function Latest({heading}) {
+  return (
+    <div id="react" className="counter">
+      <button>-</button>
+      <pre>{heading}</pre>
+      <button>+</button>
+    </div>
+  );
+}
