e7622b9cf5cf92015c82434b5d7ab2e7aea65ead
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-08-28 08:27:38 +0200

    move styling to scss modules

--- src/components/Latest.jsx
+++ src/components/Latest.jsx
@@ -1,28 +1,37 @@
+import styles from "./Latest.module.scss";
+
 export default function Latest({ latest }) {
   return (
-    <div id="react" className="counter">
-      <table>
+    <table className={styles.top}>
+      <tr>
+        <th>Region</th>
+        <th>7-dagars</th>
+        <th>ändring i %</th>
+        <th>14-dagars</th>
+      </tr>
+      {latest.rows.map((row) => (
         <tr>
-          <th>Region</th>
-          <th>7-dagars</th>
-          <th>ändring i %</th>
-          <th>14-dagars</th>
+          <th>{row.regionName}</th>
+          <td
+            className={styles.value}
+            style={{ backgroundColor: row.bgcolor }}
+          >
+            {Math.round(row.value)}
+          </td>
+          <td
+            className={styles.diff}
+            style={{ backgroundColor: row.entryDiff.bgcolor }}
+          >
+            {Math.round(row.entryDiff.value)}
+          </td>
+          <td
+            className={styles.value}
+            style={{ backgroundColor: row.entry14.bgcolor }}
+          >
+            {Math.round(row.entry14.value)}
+          </td>
         </tr>
-        {latest.rows.map((row) => (
-          <tr>
-            <th>{row.regionName}</th>
-            <td style={{ backgroundColor: row.bgcolor }}>
-              {Math.round(row.value)}
-            </td>
-            <td style={{ backgroundColor: row.entryDiff.bgcolor }}>
-              {Math.round(row.entryDiff.value)}
-            </td>
-            <td style={{ backgroundColor: row.entry14.bgcolor }}>
-              {Math.round(row.entry14.value)}
-            </td>
-          </tr>
-        ))}
-      </table>
-    </div>
+      ))}
+    </table>
   );
 }
--- src/components/Seven.jsx
+++ src/components/Seven.jsx
@@ -1,15 +1,22 @@
+import styles from "./Seven.module.scss";
+
 export default function Seven({ cases }) {
   return (
-    <div className="table">
+    <div className={styles.table}>
       <span>Statistikdatum</span>
       {cases.columns.map((region) => (
-        <span className="region">{region}</span>
+        <span className={styles.region}>{region}</span>
       ))}
       {cases.cells7.map((cellArray, index) => (
         <>
           <span>{cases.rows[index]}</span>
           {cellArray.map((cell) => (
-            <span className="cell" style={{backgroundColor:cell.bgcolor}}>{Math.round(cell.value)}</span>
+            <span
+              className={styles.cell}
+              style={{ backgroundColor: cell.bgcolor }}
+            >
+              {Math.round(cell.value)}
+            </span>
           ))}
         </>
       ))}
