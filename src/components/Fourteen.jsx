06ac53d059899df7cbf381cae4f600cd1af6deaf
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-08-31 08:14:34 +0200

    add page 14

---
+++ src/components/Fourteen.jsx
@@ -0,0 +1,25 @@
+import styles from "./Fourteen.module.scss";
+
+export default function Fourteen({ cases }) {
+  return (
+    <div className={styles.table}>
+      <span>Statistikdatum</span>
+      {cases.columns.map((region) => (
+        <span className={styles.region}>{region}</span>
+      ))}
+      {cases.cells14.map((cellArray, index) => (
+        <>
+          <span>{cases.rows[index]}</span>
+          {cellArray.map((cell) => (
+            <span
+              className={styles.cell}
+              style={{ backgroundColor: cell.bgcolor }}
+            >
+              {Math.round(cell.value)}
+            </span>
+          ))}
+        </>
+      ))}
+    </div>
+  );
+}
