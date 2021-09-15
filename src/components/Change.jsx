df1dcbc8c11c74e3ec23e867cb96368996d1e7f3
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-09-15 14:55:37 +0200

    add page "change"

---
+++ src/components/Change.jsx
@@ -0,0 +1,25 @@
+import styles from "./Change.module.scss";
+
+export default function Change({ cases }) {
+  return (
+    <div className={styles.table}>
+      <span>Statistikdatum</span>
+      {cases.columns.map((region) => (
+        <span className={styles.region}>{region}</span>
+      ))}
+      {cases.cellsDiff.map((cellArray, index) => (
+        <>
+          <span>{cases.rows[index * 7]}</span>
+          {cellArray.map((cell) => (
+            <span
+              className={styles.cell}
+              style={{ backgroundColor: cell.bgcolor }}
+            >
+              {Math.round(cell.value)}
+            </span>
+          ))}
+        </>
+      ))}
+    </div>
+  );
+}
