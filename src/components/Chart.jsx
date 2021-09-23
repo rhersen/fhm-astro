5dab4138174319fc90fca5b885fc329cf2fb036f
Rudolf Hersén <rudolf.hersen@schibsted.com>
2021-09-23 13:58:35 +0200

    add nav with (broken) links

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,32 +1,33 @@
 import styles from "./Chart.module.scss";
 
-export default function Chart({
-  dataArray: { height, max, points, region, width, yValues },
-}) {
-  console.log(region);
-  console.log(width);
-  console.log(height);
-  console.log(max);
-  console.log(yValues.length);
-  console.log(points.length);
+export default function Chart({ cases }) {
+  const { height, max, points, region, width, yValues } = cases.charts[0];
   return (
-    <div className={styles.chart}>
-      <svg viewBox={[0, 0, width, height].join(" ")}>
-        <polyline fill="none" points={points} />
-        {yValues.map((yValue) => (
-          <line
-            x1={0}
-            y1={(yValue * height) / max}
-            x2={width}
-            y2={(yValue * height) / max}
-          />
+    <>
+      <nav className={styles.regions}>
+        <a href="/">Hem</a>
+        {cases.columns.map((column) => (
+          <a href={`charts/${column.toLowerCase()}`}>{column}</a>
         ))}
-      </svg>
-      <div className={styles.yvalues}>
-        {yValues.map((yValue) => (
-          <div>{yValue}</div>
-        ))}
+      </nav>
+      <div className={styles.chart}>
+        <svg viewBox={[0, 0, width, height].join(" ")}>
+          <polyline fill="none" points={points} />
+          {yValues.map((yValue) => (
+            <line
+              x1={0}
+              y1={(yValue * height) / max}
+              x2={width}
+              y2={(yValue * height) / max}
+            />
+          ))}
+        </svg>
+        <div className={styles.yvalues}>
+          {yValues.map((yValue) => (
+            <div>{yValue}</div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 }
